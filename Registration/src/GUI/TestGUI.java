3832b088dfdf64df68625b8f564b98624155cb4e
PC <PC@DESKTOP-TK2U89B>
2020-04-20 07:49:15 +0700

    Update TestGui

---
+++ Registration/src/GUI/TestGUI.java
@@ -0,0 +1,22 @@
+package GUI;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class TestGUI extends JFrame{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TestGUI window = new TestGUI();
+	}
+	
+	public TestGUI() {
+		
+		this.setSize(500,500);
+		
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		this.setVisible(true);
+	}
+
+}
